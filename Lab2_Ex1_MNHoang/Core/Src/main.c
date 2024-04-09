52b8e6e865d573559fa1af528c5d85727b2dfe79
HoènMike <hoanggg003@gmail.com>
2024-04-09 22:26:20 +0700

    re-create Ex5

--- Lab2_Ex1_MNHoang/Core/Src/main.c
+++ Lab2_Ex1_MNHoang/Core/Src/main.c
@@ -33,7 +33,7 @@ int led_buffer[4]; // LED Status
 int hour = 15, minute = 8, second = 50;
 
 // DOT_COUNTER = 1 times per second, LED_SWITCH_COUNTER = 500 * 4 (7Seg) = 0.5Hz, CLOCK_COUNTER = 1 second counter;
-const int DOT_COUNTER = 100, LED_SWITCH_COUNTER = 50, CLOCK_COUNTER = 100;
+const int DOT_COUNTER = 100, LED_SWITCH_COUNTER = 25, CLOCK_COUNTER = 100;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
